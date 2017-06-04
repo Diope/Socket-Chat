dd23f5e5fa37bc47ff760138abad0d152bcb71d2
Dion Pettaway <dionp@fullsail.edu>
2017-06-04 12:08:19 -0400

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new guy connected');
 
-    socket.emit('newMessage', {
-        from: "Rihanna",
-        text: "Woo",
-        createdAt: new Date().getUTCFullYear()
-    })
-
     socket.on("createMessage", (message) => {
         console.log('createMessage', message)
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        }) 
     });
 
     socket.on('disconnect', () => {
